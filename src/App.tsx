e502df8d525806d12e34b873007e85384a3d079e
Jon Yardley <jonyardley@Yardleys-MacBook-Pro.local>
2020-03-28 18:11:31 +0000

    Added some styling and a reload button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import "./App.css";
 import Writer from "./components/Writer";
@@ -7,10 +8,8 @@ const App: React.FC = () => {
     <div className="app">
       <header className="app-body">
         <h1>Write or Die!</h1>
-        <p>
-          Write a story and don't stop. After two seconds you die. Deleting
-          stuff doesn't count as writing so keep those ideas flowing.
-        </p>
+        <p>Write a story and don't stop. After two seconds you die.</p>
+        <p>Deleting stuff doesn't count so keep those ideas flowing.</p>
       </header>
       <section>
         <Writer />
--- src/components/Writer.tsx
+++ src/components/Writer.tsx
@@ -42,13 +42,23 @@ const Writer: React.FC = () => {
       {hasStarted && <p>Seconds left: {Number(count).toFixed(2)}</p>}
       {!hasStarted && <p>Begin writing something...</p>}
       {hasEnded && <h1>DIE!</h1>}
+      {hasEnded && <p>Why not share your story on the social medias?</p>}
       <TextareaAutosize
         className="writer"
         autoFocus
         onKeyUp={handleKeyUp}
-        defaultValue="Once upon a time"
+        defaultValue="Once upon a time..."
         disabled={hasEnded}
       />
+
+      {hasEnded && (
+        <p>
+          <a href="#" onClick={() => window.location.reload()}>
+            Write another one.
+          </a>{" "}
+          Go on, you know you want to.
+        </p>
+      )}
     </div>
   );
 };
