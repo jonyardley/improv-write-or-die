8095e9d236c2832bc32bbf44060385a7b5c0fcc2
Jon Yardley <jonyardley@Yardleys-MacBook-Pro.local>
2020-03-28 17:58:56 +0000

    Updates

---
+++ precache-manifest.2cf6d2699581918785c3fbc3d6264f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4b393ce13bba3b8c4b443fbe53ac1b",
+    "url": "/improv-write-or-die/index.html"
+  },
+  {
+    "revision": "f67026e1af827a96e133",
+    "url": "/improv-write-or-die/static/css/main.80c1bf45.chunk.css"
+  },
+  {
+    "revision": "4f9afcbe7f3537cb0c92",
+    "url": "/improv-write-or-die/static/js/2.7a600327.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/improv-write-or-die/static/js/2.7a600327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67026e1af827a96e133",
+    "url": "/improv-write-or-die/static/js/main.cc41b43a.chunk.js"
+  },
+  {
+    "revision": "d0b1b19a033b51a8ab14",
+    "url": "/improv-write-or-die/static/js/runtime-main.3956d4ec.js"
+  }
+]);
